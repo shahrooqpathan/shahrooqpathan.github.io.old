26099948d3dc30ca9394bc6a0535ef35e8ed7ae0
Shahrooq Pathan <shahrooq.pathan@gmail.com>
2019-02-27 02:27:15 +1100

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "shahrooqpathan.github.io/precache-manifest.1549bce3e357ad6f4e56698c65514d24.js"
+  "/react-app/precache-manifest.8911a3a899ae231de5d16e7ddcf4e14c.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("shahrooqpathan.github.io/index.html", {
+workbox.routing.registerNavigationRoute("/react-app/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
